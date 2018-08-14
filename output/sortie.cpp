f13dd9ada4d233bc27ad5e9620c1f25d5a272c03
Ribodou <luc.robidou@gmail.com>
2018-08-14 19:55:55 +0200

    Update output

--- output/sortie.cpp
+++ output/sortie.cpp
@@ -1,22 +1,14 @@
 #include "Var.h"
 
 
-Var a;
-a = 2;
-Var b;
-b = 15;
-Var ma_variable_meme_avec_un_nom_vraiment_trop_long;
-ma_variable_meme_avec_un_nom_vraiment_trop_long = 568412745;
-Var a;
-a = 5;
 int main()
 {
     Var a;
     a = 3;
-
+    // pass
     Var b;
     b = 8;
+    a = 3;
     return 0;
 }
-Var a;
-a = 5;
+// pass
